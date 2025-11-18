58abd8ef944b424d767a344b029d0fc3aa98869d
Takumi Kotani <takumi59267@gmail.com>
2025-11-18 21:10:29 +0900

    solve q04

--- chapter01/q04/q04.cpp
+++ chapter01/q04/q04.cpp
@@ -1,9 +1,15 @@
-#include<stdio.h>
+#include <iostream>
+using namespace std;
+
 int main() {
     int N;
-    scanf("%d",&N);
+    cin >> N;
 
-    while(N % 2 == 1){
-        
+    //上の桁から順番に「2進法に変換した値」を求める
+    for(int x = 9; x >= 0; x--){
+        int wari = (1 << x);
+        cout << (N / wari) % 2;
     }
+    cout << endl;
+    return 0;
 }
