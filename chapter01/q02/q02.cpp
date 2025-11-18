91ce0f8722c4dac7ae40a90d025b84bebf281175
Takumi Kotani <takumi59267@gmail.com>
2025-11-18 19:48:28 +0900

    feat: solve q02 first attempt

---
+++ chapter01/q02/q02.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main () {
+    int N, X;
+    int A[];
+    int how=0;
+
+    scanf("%d %d",&N,&X);
+    for(int i=0; i<N-1;i++){
+        scanf("%d",&A[i]);
+    }
+    for(int i=0;i<N-1;i++){
+        if(A[i] == X){
+            how = 1;           
+        }
+    }
+    if(how == 1) printf("Yes");
+    else printf("No");
+}
